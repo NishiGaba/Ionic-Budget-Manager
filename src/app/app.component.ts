06cddd24ab0d47252757554bf587a1601c00435f
NishiGaba <nishi.livemedia@gmail.com>
2017-10-31 14:21:30 +0530

    Expense and Dashboard Template Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 
 import { Platform, MenuController, Nav } from 'ionic-angular';
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ListPage } from '../pages/list/list';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { AddExpensePage } from '../pages/add-expense/add-expense';
+import { ListExpensePage } from '../pages/list-expense/list-expense';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -16,7 +17,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   // make HelloIonicPage the root (or first) page
-  rootPage = HelloIonicPage;
+  rootPage = DashboardPage;
   pages: Array<{title: string, component: any}>;
 
   constructor(
@@ -29,8 +30,9 @@ export class MyApp {
 
     // set our app's pages
     this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'Dashboard', component: DashboardPage },
+      { title: 'Add Expense', component: AddExpensePage },
+      { title: 'All Expenses', component: ListExpensePage}
     ];
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { AddExpensePage } from '../pages/add-expense/add-expense';
+import { ListExpensePage } from '../pages/list-expense/list-expense';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,9 +13,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    DashboardPage,
+    AddExpensePage,
+    ListExpensePage
   ],
   imports: [
     BrowserModule,
@@ -24,9 +24,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    DashboardPage,
+    AddExpensePage,
+    ListExpensePage
   ],
   providers: [
     StatusBar,
