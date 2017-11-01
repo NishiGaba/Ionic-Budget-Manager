05b0633abd7ff4052840a084985dcbd479c29b95
NishiGaba <nishi.livemedia@gmail.com>
2017-11-01 11:09:05 +0530

    Adding Dashboard Budget Analytics

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -49,25 +49,17 @@ export class DashboardPage {
  
             type: 'doughnut',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ["Total Budget", "Total Expense"],
                 datasets: [{
                     label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [12000, 800],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        "#36A2EB",
+                        "#FF6384"
                     ],
                     hoverBackgroundColor: [
-                        "#FF6384",
                         "#36A2EB",
-                        "#FFCE56",
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56"
+                        "#FF6384"
                     ]
                 }]
             }
